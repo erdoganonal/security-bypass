2aeaadf38d2c5c55d6f5d487f568366b247f3fd4
Erdogan Önal <erdoganonal@windowslive.com>
2024-05-14 14:39:37 +0200

    GUINotificationHandler shows in the foreground

--- notification_handler/gui.py
+++ notification_handler/gui.py
@@ -17,15 +17,23 @@ _MSG_ACTION_MAP = {
 class GUINotificationHandler(NotificationHandlerBase):
     """Show messages to the user"""
 
-    def __init__(self, timeout_secs: int | None = 2000) -> None:
-        self.timeout_secs = timeout_secs
+    def __init__(self, timeout_ms: int | None = 2000) -> None:
+        self.timeout_ms = timeout_ms
 
     def show(self, message: str, title: str, msg_type: MessageType) -> None:
         root = tk.Tk()
         root.withdraw()
+        root.attributes("-topmost", 1)
+
+        if self.timeout_ms is not None:
+            root.after(self.timeout_ms, root.destroy)
+
         try:
-            if self.timeout_secs is not None:
-                root.after(self.timeout_secs, root.destroy)
-            _MSG_ACTION_MAP[msg_type](title=title, message=message)
+            _MSG_ACTION_MAP[msg_type](title=title or message, message=message, master=root)
+        except tk.TclError:
+            pass
+
+        try:
+            root.destroy()
         except tk.TclError:
             pass
