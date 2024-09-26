a233bc0628a05cee02dcd103faf32ab38af2ad4f
Erdoğan Önal <erdoganonal@windowslive.com>
2024-09-26 11:31:00 +0200

    a manager added for file exclusions

--- create_installer.py
+++ create_installer.py
@@ -1,16 +1,12 @@
 """creates a zip file for self extracting executable"""
 
-import fnmatch
-import subprocess
-import sys
-from pathlib import Path
 from typing import Generator, Iterable, TypeVar
 
 from common.tools import generate_wrapper_file
+from exclusion.manager import get_files
 from generate_all import main as generate_all
 from installer import Installer, InstallerData
 from settings import CURRENT_DIR, WRAPPER_FILE
-from updater.constants import UPDATER_FILE_NAME
 
 INSTALLER_DIR = CURRENT_DIR / "installer"
 
@@ -23,26 +19,9 @@ EXE_APP_FILENAME = CURRENT_DIR / "windows_security_bypass_installer.exe"
 
 _T = TypeVar("_T")
 
-EXCLUDED_FILES = [
-    __file__,
-    "*.bat",
-    "*.txt",
-    "*.toml",
-    "*.zip",
-    "*.sfx",
-    UPDATER_FILE_NAME,
-]
-
-EXCLUDED_FOLDERS = [
-    ".github",
-    ".vscode",
-    ".gitignore",
-    "tests",
-]
 
 ADDITIONAL = [
     WRAPPER_FILE,
-    CURRENT_DIR / "requirements.txt",
 ]
 
 
@@ -51,7 +30,7 @@ def main() -> None:
     generate_all()
     generate_wrapper_file()
 
-    files = get_files_to_archive()
+    files = get_files()
     Installer.create(
         InstallerData(name=EXE_APP_FILENAME, config_file=CONFIG_FILE, post_script=INSTALLER_SCRIPT, zip_file=ZIP_APP_FILENAME),
         _extended(files, ADDITIONAL),
@@ -61,30 +40,6 @@ def main() -> None:
 def _extended(*iterables: Iterable[_T]) -> Generator[_T, None, None]:
     for it in iterables:
         yield from it
-
-
-def _is_excluded(path: Path) -> bool:
-    for excluded_file in EXCLUDED_FILES:
-        if fnmatch.fnmatch(path.name, Path(excluded_file).name):
-            return True
-
-    return path.parts[0] in EXCLUDED_FOLDERS
-
-
-def get_files_to_archive() -> Generator[Path, None, None]:
-    """get list of all files to include in the zip file"""
-    try:
-        items = subprocess.check_output("git ls-tree -r main --name-only", text=True).splitlines()
-    except subprocess.CalledProcessError:
-        sys.exit("Error: Not a git repository or no files in the repository.")
-    except FileNotFoundError:
-        sys.exit("Error: Git is not installed.")
-
-    for item in items:
-        path = Path(item)
-        if _is_excluded(path):
-            continue
-        yield path
 
 
 if __name__ == "__main__":
